3e6bab8db9c195f17723e26e6d43cf5b53e90cc6
EBRAT HOSSEN <105649400+Ebrat222@users.noreply.github.com>
2025-06-29 19:32:57 +0600

    Create script.js

---
+++ simple-text-manager/script.js
@@ -0,0 +1,16 @@
+function addtext() {
+	if (document.getElementById("input-value").value.length === 0) return;
+	document.getElementById("text-show").value += ' ' + document.getElementById("input-value").value;
+	
+	document.getElementById("input-value").value = '';
+};
+
+function deltext() {
+	document.getElementById("text-show").value = document.getElementById("text-show").value.slice(0, -1) ;
+	
+};
+
+function actext() {
+	document.getElementById("text-show").value = '';
+	
+};
